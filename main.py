7ff620b237aa388a8654411b4972b8599efea972
OttoCh <otto.christianto.oc@gmail.com>
2022-03-22 10:11:47 +0700

    add compression

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import os
 import sys
 from PyPDF2 import PdfFileMerger
+# from PDFNetPython3.PDFNetPython import PDFDoc, Optimizer, SDFDoc, PDFNet
 
 inputDir = './input'
 outputDir = './output'
 orderFile = './order.txt'
+outputFilename = "result.pdf"
+doCompression = True
 
 orderList = []
+
+for filename in os.listdir(inputDir):
+    print(filename)
 
 # read order of merger from a file
 with open(orderFile) as f:
     contents = f.readlines()
     for line in contents:
-        orderList.append(line.strip());
+        orderList.append(line.strip())
 
 print("Order of file:")
 print(orderList)
@@ -22,5 +28,40 @@ merger = PdfFileMerger()
 for filename in orderList:
     merger.append(inputDir + '/' + filename)
 
-merger.write(outputDir + '/' + 'result.pdf')
+merger.write(outputDir + '/' + outputFilename)
 merger.close()
+
+# def get_size_format(b, factor=1024, suffix="B"):
+#     """
+#     Scale bytes to its proper byte format
+#     e.g:
+#         1253656 => '1.20MB'
+#         1253656678 => '1.17GB'
+#     """
+#     for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+#         if b < factor:
+#             return f"{b:.2f}{unit}{suffix}"
+#         b /= factor
+#     return f"{b:.2f}Y{suffix}"
+
+# if(doCompression == True):
+#     # Compress the result pdf
+#     initial_size = os.path.getsize(outputDir + '/' + outputFilename)
+#     try:
+#         # Initialize the library
+#         PDFNet.Initialize()
+#         doc = PDFDoc(outputDir + '/' + outputFilename)
+#         # Optimize PDF with the default settings
+#         doc.InitSecurityHandler()
+#         # Reduce PDF size by removing redundant information and compressing data streams
+#         Optimizer.Optimize(doc)
+#         doc.Save(outputDir + '/' + outputFilename, SDFDoc.e_linearized)
+#         doc.Close()
+#     except Exception as e:
+#         print("Error compress_file=", e)
+#         doc.Close()
+
+#     compressed_size = os.path.getsize(outputDir + '/' + outputFilename)
+#     print("Compression result:")
+#     print("Initial: " + get_size_format(initial_size))
+#     print("Compressed: " + get_size_format(compressed_size))
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Merge PDF Tool
+
+
+
+### Required Package
+
+> pip install PDFNetPython3==8.1.0
+> pip install PyPDF2
+
+### Usage
+
+1. Create `input` and `output` folder on the same folder as `main.py`
+2. Create `order.txt` on the same folder as `main.py`
+3. Insert all required PDF to `input` folder
+4. Insert order of pdf merging to `order.txt`
+5. run the script
+   > python ./main.py
+6. Find the result inside `output` folder
